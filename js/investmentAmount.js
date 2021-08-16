e1f382c3417ec60e81cd24a220b1d3706711ebec
candr006 <candr006@ucr.edu>
2021-08-16 16:20:08 -0700

    following initial tutorial to add reactjs

---
+++ js/investmentAmount.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class investmentAmount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Hello world.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#investmentAmount_component');
+ReactDOM.render(e(investmentAmount), domContainer);
