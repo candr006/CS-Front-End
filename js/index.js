508f3744ab76cbdd07e0c746354caca0b2be9f38
candr006 <candr006@ucr.edu>
2021-08-22 22:06:06 -0700

    separate js file returning form

--- js/index.js
+++ js/index.js
@@ -2,63 +2,50 @@
 
 //const e = React.createElement;
 
-	
-class preApprovalForm extends React.Component {
-	
-  constructor() {
-		super();
+
+	class PreApprovalForm extends React.Component {
+
+	  render() {
+	  	return(
+			<form class="col-lg-8 offset-lg-3 ">
+				<div class="form-group row">
+					<label class="col-sm-6 col-form-label required_label" for="investmentAmount">Investment Amount </label>
+					<div class="col-sm-9">
+						<input type="text" class="form-control" id="investmentAmount" placeholder="Ex: 25000" required />
+					</div>
+				</div>
+				<div class="form-group row">
+					<label class="col-sm-6 col-form-label required_label" for="investmentType">Investment Type </label>
+					<div class="col-sm-9">
+						<input type="text" class="form-control" id="investmentType" placeholder="Investment Type" required/>
+					</div>
+				</div>
+				<div class="form-group row">
+					<label class="col-sm-6 col-form-label required_label" for="totalNetWorth">Total Net Worth </label>
+					<div class="col-sm-9">
+						<input type="text" class="form-control" id="totalNetWorth" placeholder="Ex: 1000000" required />
+					</div>
+				</div>
+				<div class="form-group row">
+					<label class="col-sm-6 col-form-label required_label" for="estimatedYearlyIncome">User Estimated Yearly Income </label>
+					<div class="col-sm-9">
+						<input type="text" class="form-control" id="estimatedYearlyIncome" placeholder="Ex: 1000000" required />
+					</div>
+				</div>
+				<div class="form-group row">
+					<label class="col-sm-6 col-form-label required_label" for="estimatedCreditScore">User Estimated Credit Score </label>
+					<div class="col-sm-9">
+						<input type="text" class="form-control" id="estimatedCreditScore" placeholder="Ex: 1000000" required />
+					</div>
+				</div>
+				<div class="form-group row">
+					<div class="col-sm-9">
+					<button type="submit" class="btn btn-primary">Submit</button>
+					</div>
+				</div>
+			</form>
+			);
+		}
 	}
 
-  render() {
-
-  	 var html="<p>Hello World!</p>";
-
-    return ({html});
-		//index form
-			
-		/*<form class="col-lg-8 offset-lg-3 ">
-			<div class="form-group row">
-				<label class="col-sm-6 col-form-label required_label" for="investmentAmount">Investment Amount </label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="investmentAmount" placeholder="Ex: 25000" required>
-				</div>
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-6 col-form-label required_label" for="investmentType">Investment Type </label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="investmentType" placeholder="Investment Type" required>
-				</div>
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-6 col-form-label required_label" for="totalNetWorth">Total Net Worth </label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="totalNetWorth" placeholder="Ex: 1000000" required>
-				</div>
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-6 col-form-label required_label" for="estimatedYearlyIncome">User Estimated Yearly Income </label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="estimatedYearlyIncome" placeholder="Ex: 1000000" required>
-				</div>
-			</div>
-			<div class="form-group row">
-				<label class="col-sm-6 col-form-label required_label" for="estimatedCreditScore">User Estimated Credit Score </label>
-				<div class="col-sm-9">
-					<input type="text" class="form-control" id="estimatedCreditScore" placeholder="Ex: 1000000" required>
-				</div>
-			</div>
-			<div class="form-group row">
-				<div class="col-sm-9">
-				<button type="submit" class="btn btn-primary">Submit</button>
-				</div>
-			</div>
-		</form>*/
-
-    //);
-  }
-}
-
-
-//const domContainer = document.querySelector('#form_card_body');
-/*ReactDOM.render((preApprovalForm), domContainer);*/
-ReactDOM.render(new preApprovalForm(), document.querySelector('#form_card_body'));
+	ReactDOM.render(<PreApprovalForm />, document.getElementById('form_card_body'));
